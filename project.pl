ef459869d0705f8e5965b303a4ac1359293140fe
Juan Moreno <juan.andres.208@hotmail.com>
2019-02-26 00:28:21 -0500

    added two rules

--- project.pl
+++ project.pl
@@ -3,7 +3,7 @@ public(x). %Class, Method or Attribute
 protected(x). %Class, Method or Attribute
 class(c). %Define a class
 interface(c). %Define a interface
-abstract(c). %Define AbstractClass
+abstract(c). %Define Abstract
 
 method(m,c).
 attribute(a,c).
@@ -15,5 +15,11 @@ composition(a,b). %Car <o>-- Engine
 inheritance(a,b). %User <|-- Admin // User <|-- Monitor
 implements(a,b). %A implements B
 
+% Rules:
 
-error(X,Y) :- inheritance(X,Y),inheritance(Y,X).
+% Bucle
+multiplicityError(X):- class(X),multiplicity(X,X,"1");multiplicity(X,X,"1..*").
+
+extendError(A,B,C):- class(A),class(B),class(C),abstract(C),inheritance(A,C),inheritance(B,C),inheritance(A,B).
+
+extendError2(A,B) :- class(A),class(B),inheritance(A,B),multiplicity(A,B,_).
