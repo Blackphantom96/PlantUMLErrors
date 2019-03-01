dccd7d38ce983ecd9edc79a7836c30be7e5563dd
Alejandro Anzola Avila <anzola95avila@gmail.com>
2019-02-28 23:24:26 -0500

    salida de prolog sale ordenada

--- WebApp/src/main/webapp/resources/plant-uml-parser/prologConverter.py
+++ WebApp/src/main/webapp/resources/plant-uml-parser/prologConverter.py
@@ -9,23 +9,26 @@ class PrologConverter:
     """TODO"""
 
     @staticmethod
-    def write_clause(clause, outfile):
+    def write_clause(clause, out):
         """write clause"""
         if isinstance(clause, list):
             for subclause in clause:
-                PrologConverter.write_clause(subclause, outfile)
+                PrologConverter.write_clause(subclause, out)
         else:
             name = clause[0]
             attributes = clause[1:]
-            outfile.write("%s(%s).\n" % (name, ", ".join([str(i).lower() for i in attributes])))
+            out.append("%s(%s).\n" % (name, ", ".join([str(i).lower() for i in attributes])))
         
     @staticmethod
     def convert(clauses, outfile):
         """convert"""
         assert hasattr(outfile, "write"), "cannot write output file, probably not a file"
 
+        s = []
         for clause in clauses:
             for subclause in clause:
-                PrologConverter.write_clause(subclause, outfile)
+                PrologConverter.write_clause(subclause, s)
 
+        s.sort()
+        outfile.write("".join(s))
         Utilities().lprint("finished writing clauses.")
